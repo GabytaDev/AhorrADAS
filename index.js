35206ad7d579800031ff49e4e8fe9ae0b1a7b519
GabytaDev <gabyta.loyola@gmail.com>
2021-09-17 20:57:30 -0300

    comenzando seccion categorías

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 
-const balance = document.getElementById("balance")
+const botonBalance = document.getElementById("balance")
+const botonCategorias = document.getElementById("categorias")
+const botonReportes = document.getElementById("reportes")
+

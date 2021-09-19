5fb0010682fac9bab2716207696355ea394613ae
GabytaDev <gabyta.loyola@gmail.com>
2021-09-19 20:07:08 -0300

    agrego seccion categorías

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const botonBalance = document.getElementById("balance")
 const botonCategorias = document.getElementById("categorias")
 const botonReportes = document.getElementById("reportes")
 
+const seccionCategorias = document.getElementById("#categorias")
+

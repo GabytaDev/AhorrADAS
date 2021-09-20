e387792effb2e88126ee6ee3132d8193196ddca5
GabytaDev <gabyta.loyola@gmail.com>
2021-09-20 08:24:08 -0300

    funcionalidad navBar categorías y balanace

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ const botonBalance = document.getElementById("balance")
 const botonCategorias = document.getElementById("categorias")
 const botonReportes = document.getElementById("reportes")
 
-const seccionCategorias = document.getElementById("#categorias")
+let seccionCategorias = document.querySelector(".seccion-categorias")
+let seccionPortada = document.querySelector(".seccion-portada")
 
+seccionCategorias.style.display = "none"
+
+botonCategorias.onclick = () => {
+    seccionPortada.style.display = "none"
+    seccionCategorias.style.display = "block"  
+}
+
+botonBalance.onclick = () => {
+    seccionPortada.style.display = "block"
+    seccionCategorias.style.display = "none"
+}

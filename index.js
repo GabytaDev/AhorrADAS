8131cd9047ce37e18000f8c7b925aa21d3ea24a1
GabytaDev <gabyta.loyola@gmail.com>
2021-09-20 15:24:04 -0300

    agrego funcionalidad reportes, categorias y balance en navbar

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const botonReportes = document.getElementById("reportes")
 
 let seccionCategorias = document.querySelector(".seccion-categorias")
 let seccionPortada = document.querySelector(".seccion-portada")
+let seccionReportes = document.querySelector(".seccion-reportes")
 
 seccionCategorias.style.display = "none"
+seccionReportes.style.display = "none"
 
 botonCategorias.onclick = () => {
     seccionPortada.style.display = "none"
@@ -16,4 +18,12 @@ botonCategorias.onclick = () => {
 botonBalance.onclick = () => {
     seccionPortada.style.display = "block"
     seccionCategorias.style.display = "none"
+    seccionReportes.style.display = "none"
+}
+
+botonReportes.onclick = () => {
+    seccionReportes.style.display = "block"
+    seccionPortada.style.display = "none"
+    seccionCategorias.style.display = "none"
+
 }
